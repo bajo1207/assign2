cb14fd34ac358b73bac4c4b087a2b5150dfd1cd6
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2020-12-07 12:24:05 +0100

    Final

--- GOenrich.py
+++ GOenrich.py
@@ -4,7 +4,6 @@ MAIN TASK: Calculate the enrichment of specific GO terms in a list of selected g
 
 authors: Joachim Bache-Mathiesen, Antonietta Salerno
 date: 9/12/2020
-
 '''
 
 from GOenrich_utils import *
@@ -12,7 +11,7 @@ from GOenrich_utils import *
 # Read GO annotations and create a list of dictionaries, where every element is a line
 gotext = open("GOannotations.txt", "r")
 goannotations = [] # list of dictionaries with the split lines
-allgenes = [] # all genes having a GO term (coding genes)
+allgenes = set() # all genes having a GO term
 unique_set_of_goterms = set() # unique set of all GO IDs
 next(gotext) # skip first line: columns titles
 for line in gotext:
@@ -25,14 +24,15 @@ for line in gotext:
     goannotations.append(item)
     
     ''' Remark 1:
-     We decided to directly examine those genes that is ceirtan to have a GO term associated in order to reduce the compute time.
+     We decided to directly examine those genes that is certain to have a GO term associated in order to reduce the computational time.
      The choice was also driven by the fact that we noticed that there are gene IDs that exist in the GOannotations file but not in allGenes, 
      this does not only provide a less reliable statistic but also introduces the computational problem of the division by 0.
      '''
     
-    allgenes.append(item["gene_stab_id"]) # Store all genes found in the GOannotation file
+    allgenes.add(item["gene_stab_id"]) # Store all genes found in the GOannotation file
     unique_set_of_goterms.add(item['go_term']) 
 unique_list_of_goterms = list(unique_set_of_goterms) # Create a list containing all the GO terms
+allgenes = list(allgenes)
 gotext.close()
 
 selected_genes = set(create_list("IDS2.txt")) # Read the file containing the selected genes and transform it into a set to avoid duplicates
@@ -45,15 +45,12 @@ for gene in allgenes:
 Remark 2: One problem that could have arised when using the GOannotations.txt file instead of allGenes.txt is that there could have been some genes 
 that are differentially expressed (and therefore found in the IDS2.txt) but their function is unknown (no GO term associated). 
 For that reason we found it useful to check if all the selected genes were targeted as such in the allgenes_boolean_selected dictionary.
-
 GO_selected=[]
 for gene in allgenes_boolean_selected: 
     if allgenes_boolean_selected[gene]==True:
         GO_selected.append(gene)
 print(len(GO_selected))
-
 print(len(selected_genes))
-
 In this case all 119 genes present in the IDS2.txt file are found in the GOannotations.txt file.
 '''
 
@@ -61,8 +58,7 @@ In this case all 119 genes present in the IDS2.txt file are found in the GOannot
     
 """
 In order to see if there are more occurrences of a GO term in the gene selection than expected 
-the genes associated to that category as well as the genes that are absent or precent in the selection are counted.
-
+the genes associated to that category as well as the genes that are absent or present in the selection are counted.
 We generate a new file which contains the ID, name and chi-squared value grouped by category.
 The chi-squared value is an indicator of how much that GO term is overrepresented in the selection.
 """
@@ -103,4 +99,3 @@ for go_term in unique_list_of_goterms:
 newlist = sorted(list_of_go_terms_with_chi2, key=lambda k: k['chi2'])
 for item in newlist[-5:]:
     print(item)
-
